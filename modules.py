7b17cfc2055104feec06211ceafbb17df84ebdbb
SleepyMarten <Atichoke@live.se>
2021-01-29 06:40:37 +0100

    upload labb2

--- modules.py
+++ modules.py
@@ -1,30 +1,138 @@
-from random import randint
+import csv
+import json
 
-#Input 2 numbers and it will find number from 0-1000 that is divideable Integers.
-def heltal(input1, input2):
-    numbers = []
-    for num in range(1, 1001):
-        if(num/input1).is_integer() & (num/input2).is_integer():
-            numbers.append(num)
-    return numbers #Returning a list of divideable integers.
-
-#randomizing a number from 1 to 100 for the user to guess.
-def guess_function():
-    rnd = randint(1, 101) #randomizing number to guess.
-    print("\nHINT (do not look): %s\n" % (rnd)) #using this to confirm what number the rnd has randomized.
-    print("Welcome to the guess the number funtion.")
-    print("You will have to guess a number bewteen number 1 and 100.\n")
-    while (True):
-        user_input = input('Input the number you think is right: ')
+# open the csv file.
+def read_csv(csv_file):
+    while True:
         try:
-            user_answer = int(user_input)
-            if user_answer == rnd:
-                print("\nCongratulation! You guessed right!\n")
-                input("Press any key to continue.")
+            with open(csv_file, newline='', encoding='utf-8-sig') as f:
+                reader = csv.DictReader(f, delimiter=';')
+                for row in reader:
+                    print(row)
+                input("\nTryck Enter för att forstätta.")
                 break
-            elif user_answer < rnd:
-                print("WRONG! You guessed to LOW, try to guess HIGHER.\n")
-            else:
-                print("WRONG! You guessed to HIGH, try to guess LOWER.\n")
-        except ValueError:
-            print("Wrong input. Try again.\n")
+        # If the CSV file is not found, will have to manually input the path for it.
+        except FileNotFoundError:
+            print("\nKunde inte hitta CSV filen. Vänligen mata in sökväg för CSV filen.")
+            # User input the filepath for the CSV file.
+            csv_file = input("CSV sökväg: ")
+
+
+# save the csv file as dictionary in list then as JSON file.
+def save_csv(csv_file, json_file):
+    while True:
+        try:
+            with open(csv_file, newline='', encoding='utf-8-sig') as f:
+                reader = csv.DictReader(f, delimiter=';')
+                data = []
+                for row in reader:
+                    data.append(row)
+            with open(json_file, 'w', encoding='utf-8-sig') as f:
+                # saving the json file.
+                json.dump(data, f, indent=4, ensure_ascii=False)
+            print("\nCSV fil sparat som %s" % (json_file))
+            input("Tryck Enter för att forstätta.")
+            break
+        except FileNotFoundError:
+            print("\nKunde inte hitta CSV filen. Vänligen mata in sökväg för CSV filen.")
+            csv_file = input("CSV sökväg: ")
+
+
+# adding a student to the json file.
+def add_student(json_file):
+    try:
+        with open(json_file, newline='', encoding='utf-8-sig') as f:
+            data = json.load(f)
+            Användarnamn = ""
+            Förnamn = ""
+            Efternamn = ""
+            epost = ""
+
+            print("\nAnge information om studenten: \n")
+            # check so that the user doesn't input an exmpty string. // credits to Mattias.
+            while not Användarnamn:
+                # strip to remove "SPACE"
+                Användarnamn = input("Användarnamn: ").strip()
+            while not Förnamn:
+                Förnamn = input("Förnamn: ")
+            while not Efternamn:
+                Efternamn = input("Efternamn: ")
+            while not epost:
+                epost = input("epost: ")
+            # Adding the student into a dictionary.
+            student = {'Användarnamn': Användarnamn, 'Förnamn': Förnamn,
+'Efternamn': Efternamn, 'epost': epost}
+            data.append(student)  # add student to data List.
+
+            while True:
+                print("\nVill du lägga till %s till student listan?" % (Förnamn))
+                inputed = input("Mata in 'ja' eller 'nej'.\n").lower()
+                if inputed == 'ja':
+                    with open(json_file, 'w', encoding='utf-8-sig') as f:
+                        # saving the data of student to the json file.
+                        json.dump(data, f, indent=4, ensure_ascii=False)
+                    print("Information om %s har blivit tillagd i %s!" %
+                          (Förnamn, json_file))
+                    input("Tryck Enter för att forstätta.")
+                    break
+                elif inputed == 'nej':
+                    print("\nInformation om %s blev inte tillagd." %
+                          (Användarnamn))
+                    input("Tryck Enter för att forstätta.")
+                    break
+                else:
+                    print(
+                        "Mata in 'ja' eller 'nej' för att spara eller inte spara denna student till student listan.")
+    except FileNotFoundError:
+        print("Kunde inte hitta JSON filen. Vänligen konrollera JSON_FILE.")
+        input("Tryck ENTER för att återgå till menyn.")
+
+
+# remove a student from the student_data.json.
+def remove_student(json_file):
+    while True:
+        try:
+            with open(json_file, 'r', encoding='utf-8-sig') as f:
+                reader = json.load(f)
+                # so the first student number is started with 0. ( for the del reader(inputed) part)
+                counter = -1
+                for s in reader:
+                    counter += 1
+                    print("[%s] %s %s %s %s" % (counter, s['Användarnamn'],s['Förnamn'], s['Efternamn'], s['epost']))
+                print("\nAnge nummer för vilken student som ska raderas från student listan.")
+                while True:
+                    try:
+                        inputed = int(input("Student nummer: "))
+                        del reader[inputed]
+                        input("Tryck Enter för att forstätta.")
+                        break
+                    except ValueError:
+                        print("Vänligen mata in ett student nummer.")
+                    except IndexError:
+                        print("Student nummer existerar inte!")
+            with open(json_file, 'w', encoding='utf-8-sig') as f:
+                # saving the json file.
+                json.dump(reader, f, indent=4, ensure_ascii=False)
+                break
+        except FileNotFoundError:
+            print(
+                "\nKunde inte hitta JSON filen. Vänligen mata in sökväg för JSON filen.")
+            json_file = input("Json sökväg: ")
+
+
+# show a list of student from the studnet_data.json
+def show_student(json_file):
+    while True:
+        try:
+            with open(json_file, 'r', encoding='utf-8-sig') as f:
+                reader = json.load(f)
+                counter = 0 # to count how many students exist in the list.
+                for s in reader:
+                    counter += 1
+                    print("[%s] %s %s %s %s" % (counter, s['Användarnamn'],s['Förnamn'], s['Efternamn'], s['epost']))
+                input("\nTryck Enter för att forstätta.")
+                break
+        except FileNotFoundError:
+            print(
+                "\nKunde inte hitta JSON filen. Vänligen mata in sökväg för JSON filen.")
+            json_file = input("Json sökväg: ")
--- program.py
+++ program.py
@@ -1,36 +1,46 @@
-#LAB_1
+import csv, json, modules
 
-from modules import guess_function, heltal #imported from modules.py
+#Note that I comment in English because it is easier.
+#The Program UI however is in Swedish so other people in my house can test the program.
 
+csv_file = 'labb2-personer.csv'
+json_file = 'student_data.json'
 while True:
-    #Print menu
+    #Print out the menu
     print("\n---------------------MENU---------------------")
-    print("Choose a function to use\n")
-    print("(1) Heltal function.")
-    print("(2) Guess the number function.")
-    print("(3) Exit.")
+    print("Välj en funktion\n")
+    print("(1) Visa CSV listan.")
+    print("(2) Spara CSV listan som JSON fil.\n")
+    print("(3) Mata in information om en student.")
+    print("(4) Ta bort en student från student listan.")
+    print("(5) Visa lista av studenter.\n")
+    print("\n(q) AVSLUTA PROGRAMMET.")
     print("----------------------------------------------\n")
-
-    inputed = input("Choose a function: ")
-    if inputed == '1': #starting the first function ( heltal )
-        print("Welcome to the heltal function.")
-        print("You need to input (2) numbers to continue.")
-        while True:
-            try:
-                input1 = int(input("Input 1st number: ")) #user input of the 1st number
-                input2 = int(input("Input 2nd number: ")) #user input of the 2nd number
-                print("You have inputed: %s and %s" %(input1, input2)) #showing what number the user inputed.
-                print("output: ")
-                print(heltal(input1, input2)) #output of which integers are divideable
-                input("Press any key to continue.")
-                break
-            except ValueError:
-                print("Try again.")
-    elif inputed == '2':
-        guess_function()
-    elif inputed == '3': #Exiting the program
-        print("Shuting down program.")
-        input("Press any key to continue.")
+    
+    inputed = input("VÄLJ EN FUNKTION: ")
+    if inputed =='1': #Show the CSV list.
+        print("Öppnar och visar CSV filen: \n")
+        modules.read_csv(csv_file)
+        
+    elif inputed =='2': # saveing the CSV list as JSON file ( dict in a list).
+        print("Konverterar CSV filen till JSON fil......")
+        modules.save_csv(csv_file, json_file)
+        
+    elif inputed == '3': # Add student and student information to the student list(student_data.json)
+        print("Mata in information om studenten som ska läggas in till student listan.")
+        modules.add_student(json_file)
+        
+    elif inputed == '4': # Remove student information from the student list.
+        print("Välj student som ska raders från student listan.")
+        modules.remove_student(json_file)
+        
+    elif inputed == '5': # Showing a list of students.
+        print("Visar en lista av studenter.\n")
+        modules.show_student(json_file)
+        
+    elif inputed == 'q': # Exiting the program.
+        print("Programmet avslutas.")
+        input("Tryck Enter för att forstsätta.")
         break
     else:
-        print("Try again.")
+        print("Vänligen välj en funktion ( 1-5 ) ")
